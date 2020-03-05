ed1b9d9c61dead091e97e1049362e46dfa5cf805
AnaSilviaSerban <anasilviaserban@gmail.com>
2020-03-05 15:20:40 +0000

    Fasten up the algorithm

--- SoftCosineSimilarity.py
+++ SoftCosineSimilarity.py
@@ -1,4 +1,3 @@
-from allennlp.predictors import predictor
 from gensim import corpora
 import gensim.downloader as api
 from gensim.models import WordEmbeddingSimilarityIndex
@@ -10,6 +9,7 @@ from operator import itemgetter
 from allennlp.predictors.predictor import Predictor
 import os
 import sqlite3
+from nltk.stem import WordNetLemmatizer
 
 # connecting to the database
 connection = sqlite3.connect("LegalCorpus.db")
@@ -18,19 +18,27 @@ connection = sqlite3.connect("LegalCorpus.db")
 crsr = connection.cursor()
 
 
+# model = api.load("fasttext-wiki-news-subwords-300")
+# index = WordEmbeddingSimilarityIndex(model)
+# filename_similarity = 'sparse_model.pkl'
+# index.save(filename_similarity)
+
+
 class SoftCosineSimilarity(object):
     # TODO try to speed up
-    model = api.load("fasttext-wiki-news-subwords-300")
-    # question = input()
-    question = 'Who is the owner of the "Software" licensed under this Agreement?'
-
+    # Lemmatize and tokenize the words (simple_preprocess)
+    # Loops through the lists in the document and tokenizes the word
+    # while removing stopwords
     def remove_stopwords(self, documents_array, stop_words, documents):
+        lemmatizer = WordNetLemmatizer()
         for doc in documents:
             temp = []
             for w in simple_preprocess(doc):
                 if w not in stop_words:
-                    temp.append(w)
+                    temp.append(lemmatizer.lemmatize(w))
             documents_array.append(temp)
+
+    """Convert `paragraphs` into the bag-of-words (BoW) format = list of `(token_id, token_count)` tuples."""
 
     def convert_sentences(self, sentences, dictionary, documents_array):
         for paragraph in documents_array:
@@ -38,15 +46,20 @@ class SoftCosineSimilarity(object):
             if sent_doc != []:
                 sentences.append(sent_doc)
 
+    """Iterate through directory and return a list of names of all .txt files in it."""
+
     def get_files_names(self, dir_path):
-        """Iterate through directory and return a list of names of all .txt files in it."""
         return [file_name for file_name in os.listdir(dir_path) if file_name.endswith('.txt')]
 
     def retrieve_paragraphs(self, file_name, question_input):
-        with open(file_name, encoding="ISO-8859-1") as fp:
+        # Reading file names and read them paragraph by paragraph.
+        with open(file_name, "r") as fp:
             documents = fp.readlines()
 
+        # Append question to the end of the document and pre-process everything all at once.
         documents.append(question_input)
+
+        # convert everything to lower case for better processing.
         documents = [doc.lower() for doc in documents]
 
         # Remove stopwords
@@ -56,6 +69,7 @@ class SoftCosineSimilarity(object):
 
         # Prepare a dictionary and a corpus.
         # Convert a document into a list of tokens.
+        # This module implements the concept of a Dictionary -- a mapping between paragraphs and their integer ids.
         dictionary = corpora.Dictionary([word for word in documents_array])
 
         # Convert the sentences into bag-of-words vectors.
@@ -63,7 +77,8 @@ class SoftCosineSimilarity(object):
         self.convert_sentences(sentences, dictionary, documents_array)
 
         # Another way of computing the similarity matrix
-        similarity_index = WordEmbeddingSimilarityIndex(self.model)
+        # similarity_index = WordEmbeddingSimilarityIndex(self.loaded_model)
+        similarity_index = WordEmbeddingSimilarityIndex.load('sparse_model.pkl')
         similarity_matrix = SparseTermSimilarityMatrix(similarity_index, dictionary)
 
         result_matrix = []
@@ -72,42 +87,53 @@ class SoftCosineSimilarity(object):
         for sentence in sentences:
             result_matrix.append(similarity_matrix.inner_product(sentences[-1], sentence, normalized=True))
 
+        # print(" -- Similarity between document 1 and document 2: -- ")
+        # for sentence in sentences:
+        #     print(rank(sentences[-1], sentence))
+        #     print(sentence)
+        # print(" ------ ")
+
         for index, result in enumerate(result_matrix):
             result_dict[index] = result
 
         result_dict = OrderedDict(sorted(result_dict.items(), key=itemgetter(1), reverse=True))
-        para_index = list(result_dict.keys())[1]
+        para_index = [list(result_dict.keys())[1], list(result_dict.keys())[2], list(result_dict.keys())[3],
+                      list(result_dict.keys())[4]]
 
         print(" -- FILE NAME: ", file_name)
         similarity_value = list(result_dict.values())[1]
         count = 0
-        doc_paragraph = []
+        doc_paragraph = ''
         for doc in documents:
             if doc != '\n':
-                if count == para_index:
+                if count in para_index:
                     print(" -- This is the paragraph: ", similarity_value)
                     print(doc)
-                    doc_paragraph = doc
+                    doc_paragraph = doc_paragraph + ' ' + doc
                 count += 1
 
-        predictor = Predictor.from_path(
-            "https://storage.googleapis.com/allennlp-public-models/bidaf-elmo-model-2018.11.30-charpad.tar.gz")
-        prediction = predictor.predict(
-            question=question_input,
-            passage=doc_paragraph
-        )
+        answer = "There is no answer for this document!"
         print(" -- This is the span of words:")
-        print(prediction["best_span_str"])
-        answer = prediction["best_span_str"]
-        print(type(similarity_value))
-
+        if similarity_value > 0.3:
+            predictor = Predictor.from_path(
+                "https://storage.googleapis.com/allennlp-public-models/bidaf-elmo-model-2018.11.30-charpad.tar.gz")
+            prediction = predictor.predict(
+                question=question_input,
+                passage=doc_paragraph
+            )
+            print(prediction["best_span_str"])
+            answer = prediction["best_span_str"]
+        else:
+            print(answer)
         print("------------------------")
         # SQL command to insert the data in the table
         sql_command = """INSERT INTO law_documents (filename, similarityValue, paragraphExtracted, smallAnswer) 
         VALUES (?, ?, ?, ?); """
+
         crsr.execute(sql_command, (file_name, float(similarity_value), doc_paragraph, answer))
 
-"""while"""
+
+"""while true"""
 
 softCosineSimilarity = SoftCosineSimilarity()
 
@@ -127,4 +153,3 @@ while True:
         print(i)
 
     # close the connection
-
