9ea9ff47a236f9efcb26a7e16543c438d4856f8e
AnaSilviaSerban <anasilviaserban@gmail.com>
2020-04-27 11:52:21 +0100

    uploaded more tests

--- test_softCosineSimilarity.py
+++ test_softCosineSimilarity.py
@@ -12,16 +12,46 @@ class TestCosine(unittest.TestCase):
 
 class TestSoftCosineSimilarity(TestCosine):
     def test_remove_stopwords(self):
-        document = "These words are said to have a very low discrimination value when it comes to IR and they are " \
-                   "known as stopwords or sometimes as noise words or the negative dictionary. "
+        document = ['Microsoft Software License terms\n', '\n', 'WINDOWS operating system\n', '\n',
+                    'if you live in (or if your principal place of business is in) the united states, please read the binding arbitration clause and class action waiver in section 11. it affects how disputes are resolved.\n',
+                    '\n', 'thank you for choosing microsoft!\n']
 
         stop_words = set(stopwords.words('english'))
         documents_array = []
-        self.softCosineSimilarity.remove_stopwords(documents_array, stop_words, document)
-        self.assertNotEquals(document, documents_array)
+        documents_array = self.softCosineSimilarity.remove_stopwords(documents_array, stop_words, document)
+        self.assertEquals([['microsoft', 'software', 'license', 'term'], [], ['window', 'operate', 'system'], [], ['if', 'you', 'live', 'in', 'or', 'if', 'your', 'principal', 'place', 'of', 'business', 'be', 'in', 'the', 'united', 'state', 'please', 'read', 'the', 'bind', 'arbitration', 'clause', 'and', 'class', 'action', 'waiver', 'in', 'section', 'it', 'affect', 'how', 'dispute', 'be', 'resolve'], [], ['thank', 'you', 'for', 'choose', 'microsoft']], documents_array)
 
-    def test_convert_sentences(self):
-        self.fail()
+    def test_lowercase_words(self):
+        isLower = True
+        document = ['Microsoft Software License terms\n', '\n', 'WINDOWS operating system\n', '\n',
+                    'if you live in (or if your principal place of business is in) the united states, please read the binding arbitration clause and class action waiver in section 11. it affects how disputes are resolved.\n',
+                    '\n', 'thank you for choosing microsoft!\n']
 
-    def test_retrieve_paragraphs(self):
-        self.fail()
+        stop_words = set(stopwords.words('english'))
+        documents_array = []
+        documents_array = self.softCosineSimilarity.remove_stopwords(documents_array, stop_words, document)
+        for doc in documents_array:
+            for word in doc:
+                if word.islower():
+                    pass
+                else:
+                    isLower = False
+                    break
+        self.assertTrue(isLower)
+
+
+    def test_lemmatize_words(self):
+        isLemmatized = False
+        document = ['Playing Microsoft Software License terms\n', '\n', 'WINDOWS operating system\n', '\n',
+                    'if you live in (or if your principal place of business is in) the united states, please read the binding arbitration clause and class action waiver in section 11. it affects how disputes are resolved.\n',
+                    '\n', 'thank you for choosing microsoft!\n']
+
+        stop_words = set(stopwords.words('english'))
+        documents_array = []
+        documents_array = self.softCosineSimilarity.remove_stopwords(documents_array, stop_words, document)
+        for doc in documents_array:
+            for word in doc:
+                if word == 'play':
+                    isLemmatized = True
+
+        self.assertTrue(isLemmatized)
